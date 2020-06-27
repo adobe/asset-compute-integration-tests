17e8b3c7c29946d7089cbedf1fd85fcc17bb4b67
Alexander Klimetschek <aklimets@adobe.com>
2020-06-26 19:30:17 -0700

    NUI-633 Integration tests for basic developer experience

--- test/index.test.js
+++ test/index.test.js
@@ -12,9 +12,69 @@
 
 'use strict';
 
+const assert = require("assert");
+const { execSync } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+const rimraf = require("rimraf");
+
+function shell(command, dir) {
+    command = command
+        .split("\n")
+        .map(line => line.trim())
+        .filter(line => line.length > 0)
+        .filter(line => !line.startsWith("#"))
+        .join(";");
+    execSync(command, {cwd: dir, stdio: 'inherit'});
+}
+
+// create dir (if doesn't exist yet) and change into it
+function cd(dir) {
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
+    }
+    process.chdir(dir);
+}
+
 describe("integration tests", function() {
 
-    it("should install tools and run developer experience", async function() {
-        console.log("TODO implement");
+    const BUILD_DIR = path.resolve("build"); // absolute paths required beloow
+
+    beforeEach(function() {
+        this.timeout(30000);
+
+        // make npm global installations go into a specific directory
+        // to avoid messing with a user's actual global npm installations
+        process.env.NPM_CONFIG_PREFIX=`${BUILD_DIR}/npm`;
+        process.env.PATH=`${process.env.NPM_CONFIG_PREFIX}/bin:${process.env.PATH}`;
+
+        // reset/clear build dir entirely before each run
+        rimraf.sync(BUILD_DIR);
+
+        cd(BUILD_DIR);
     });
+
+    it("should install tools and run developer experience", async function() {
+        shell(`
+            npm install -g @adobe/aio-cli
+            aio update --no-confirm
+        `);
+
+        cd("project");
+
+        // TODO: get default -y selection to include asset compute worker OR interactive input
+
+        shell(`
+            aio app init --no-login -y --asset-compute -i ../../test/console.json
+        `);
+
+        assert(fs.existsSync(path.join("actions", "generic", "index.js")));
+
+        // TODO: validate asset compute worker created once available
+        // assert(fs.existsSync(path.join("actions", "worker", "index.js")));
+
+        shell(`
+            aio app test
+        `);
+    }).timeout(300000);
 });
