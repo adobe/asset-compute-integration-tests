3f87b84aba76e5f0b5c39a233a6528c19bf91e0a
delbick <delbick@adobe.com>
2021-09-20 14:16:04 -0700

    remove travis ci

--- test/index.test.js
+++ test/index.test.js
@@ -87,25 +87,20 @@ describe("integration tests", function() {
         shell('ls');
         assert(fs.existsSync(path.join("src", "dx-asset-compute-worker-1", "actions", "worker", "index.js")));
 
-        if (process.env.TRAVIS && os.platform() === "win32") {
-            console.log("SKIPPING aio app test on Travis Windows (docker linux containers required for worker tests)");
+        const testLogsFile = path.join("build", "test-results", "test-worker", "test.log");
+        assert.ok(!fs.existsSync(testLogsFile));
+        shell(`
+            aio app test
+        `);
+        assert.ok(fs.existsSync(testLogsFile));
+        const testLogs = fs.readFileSync(testLogsFile);
+        assert.ok(testLogs.includes('Validation successful'));
 
-        } else {
-            const testLogsFile = path.join("build", "test-results", "test-worker", "test.log");
-            assert.ok(!fs.existsSync(testLogsFile));
-            shell(`
-                aio app test
-            `);
-            assert.ok(fs.existsSync(testLogsFile));
-            const testLogs = fs.readFileSync(testLogsFile);
-            assert.ok(testLogs.includes('Validation successful'));
-
-            // test as aio plugin
-            shell(`
-                aio plugins:install @adobe/aio-cli-plugin-asset-compute
-                aio asset-compute test-worker
-            `);
-        }
+        // test as aio plugin
+        shell(`
+            aio plugins:install @adobe/aio-cli-plugin-asset-compute
+            aio asset-compute test-worker
+        `);
     }).timeout(600000);
     it("should install version 7.1.0 of aio-cli and run developer experience", async function() {
         shell(`
@@ -133,24 +128,19 @@ describe("integration tests", function() {
 
         assert(fs.existsSync(path.join("actions", "worker", "index.js")));
 
-        if (process.env.TRAVIS && os.platform() === "win32") {
-            console.log("SKIPPING aio app test on Travis Windows (docker linux containers required for worker tests)");
+        const testLogsFile = path.join("build", "test-results", "test-worker", "test.log");
+        assert.ok(!fs.existsSync(testLogsFile));
+        shell(`
+            aio app test
+        `);
+        assert.ok(fs.existsSync(testLogsFile));
+        const testLogs = fs.readFileSync(testLogsFile);
+        assert.ok(testLogs.includes('Validation successful'));
 
-        } else {
-            const testLogsFile = path.join("build", "test-results", "test-worker", "test.log");
-            assert.ok(!fs.existsSync(testLogsFile));
-            shell(`
-                aio app test
-            `);
-            assert.ok(fs.existsSync(testLogsFile));
-            const testLogs = fs.readFileSync(testLogsFile);
-            assert.ok(testLogs.includes('Validation successful'));
-
-            // test as aio plugin
-            shell(`
-                aio plugins:install @adobe/aio-cli-plugin-asset-compute
-                aio asset-compute test-worker
-            `);
-        }
+        // test as aio plugin
+        shell(`
+            aio plugins:install @adobe/aio-cli-plugin-asset-compute
+            aio asset-compute test-worker
+        `);
     }).timeout(600000);
 });
